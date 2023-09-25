1fd443439f77744060095cce34e9c9ff08b77820
Yusuf Can Sağlam <yusufcan.sglm@hotmail.com>
2023-09-25 14:52:26 +0300

    Initial Commit

---
+++ HeroSpace/HeroSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroSpaceApp.swift
+//  HeroSpace
+//
+//  Created by Yusuf Can Sağlam on 25.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
